e9f8756f754cde9ab1605264dc43e869c5526b59
Carlos Fernandes <69088161+carlosfernandescrypt@users.noreply.github.com>
2024-05-09 13:18:11 -0300

    adicionado main.h

---
+++ main.h
@@ -0,0 +1,35 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_HOSPEDES 400
+#define MAX_NOME 50
+#define MAX_QUARTOS 100
+#define MAX_HOSPEDES_POR_QUARTO 4
+
+typedef struct {
+    char nome[MAX_NOME];
+    int quarto;
+} Hospede;
+
+extern Hospede hotel[MAX_HOSPEDES];
+extern int totalHospedes;
+extern int quartos[MAX_QUARTOS];
+
+void inicializarQuartos(); // 1
+void inserirHospede(char *nome, int quarto);
+void lerHospede();
+void mostrarQuartosVazios();
+void ordenarHospedes();
+void listarHospedes();
+void buscarHospede(char *nome);
+void editarHospede(char *nome, char *novoNome, int novoQuarto);
+void liberarQuarto(int quarto);
+void salvarHospedes();
+void atualizarLista();
+void limparArquivo();
+
+#endif // MAIN_H
