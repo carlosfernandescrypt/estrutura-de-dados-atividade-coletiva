d142d2d6a766d74c61297d4b8769db5fc11115f4
Carlos Fernandes <69088161+carlosfernandescrypt@users.noreply.github.com>
2024-05-09 22:12:13 -0300

    Código comentado e arrumei função liberarQuarto

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
+// Definindo as bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
 
+// Definindo a estrutura do nó da lista 
 #define MAX_HOSPEDES 400 
 #define MAX_NOME 50 
 #define MAX_QUARTOS 100
 #define MAX_HOSPEDES_POR_QUARTO 4
 
-typedef struct {
+// Definindo a struct do Hóspede com dois atributos (nome e quarto)
+typedef struct Hospede {
     char nome[MAX_NOME];
     int quarto;
 } Hospede;
 
+// Declaração da estrutura para armazenar informações dos hóspedes, juntamente com a variável para rastrear o total de hóspedes no hotel e um array para controlar a ocupação dos quartos no hotel
+Hospede hotel[MAX_HOSPEDES];
+int totalHospedes = 0;
+int quartos[MAX_QUARTOS];
+
+// Função para inicializar os quartos do hotel com o valor 0, indicando que estão vazios
 void inicializarQuartos() {
     int i;
     for (i = 0; i < MAX_QUARTOS; i++) {
@@ -20,6 +29,7 @@ void inicializarQuartos() {
     }
 }
 
+// Função para inserir um novo hóspede no Hotel, primeiro ela verifica se o hotel está cheio, caso esteja, ela exibe uma mensagem de erro, caso esteja vazio, ela pede ao usuário o nome e o quarto do hóspede, e então insere o hóspede no hotel
 void inserirHospede(char *nome, int quarto) {
     if (totalHospedes >= MAX_HOSPEDES) {
         printf("Hotel cheio, não é possível inserir mais hóspedes.\n");
@@ -33,11 +43,12 @@ void inserirHospede(char *nome, int quarto) {
 
     strcpy(hotel[totalHospedes].nome, nome);
     hotel[totalHospedes].quarto = quarto;
-    totalHospedes++
+    totalHospedes++;
 
     quartos[quarto]++;
 }
 
+// Função para ler o arquivo de entrada e inserir os hóspedes no Hotel
 void lerHospede() { 
     FILE *file = fopen("hospedes.txt", "r");
     char linha[MAX_NOME + 20];
@@ -60,6 +71,7 @@ void lerHospede() {
     printf("Hóspedes lidos do arquivo 'hospedes.txt' e inseridos no hotel.\n");
 }
 
+// Função para mostrar o quartos que estão vazios no Hotel
 void mostrarQuartosVazios() {
     int i;
     printf("Quartos vazios: ");
@@ -71,6 +83,7 @@ void mostrarQuartosVazios() {
     printf("\n");
 }
 
+// Função para ordernar os hóspedes do Hotel pelo nome
 void ordenarHospedes() {
     int i, j;
     Hospede temp;
@@ -86,6 +99,7 @@ void ordenarHospedes() {
     }
 }
 
+// Função para mostrar os hóspedes do Hotel, ela chama primeiramente a função de ordernarHospedes para mostrar eles em ordem alfabetica.
 void listarHospedes() {
     int i;
 
@@ -94,7 +108,9 @@ void listarHospedes() {
     for (i = 0; i < totalHospedes; i++) {
         printf("Hóspede: %s, Quarto: %d\n", hotel[i].nome, hotel[i].quarto);
     }
+}
 
+// Função para buscar um hóspede no hotel pelo nome, caso o Hóspede esteja cadastrado, será retornado seu nome e o seu respectivo quarto, caso contrário, será exibido uma mensagem de erro.
 void buscarHospede(char *nome) {
     int i;
     int encontrado = 0;
@@ -110,13 +126,15 @@ void buscarHospede(char *nome) {
     if (!encontrado) {
         printf("Hóspede %s não encontrado.\n", nome);
     }
+}
 
 
+// Função para editar um Hóspede, caso o hóspede esteja cadastrado, será solicitado o novo nome do hóspede (caso necessario, senão é só manter o nome) e o novo número do quarto (caso necessario, senão é só manter o número)
 void editarHospede(char *nome, char *novoNome, int novoQuarto) {
     int i;
     int encontrado = 0;
     int temp;
-    
+
     for (i = 0; i < totalHospedes; i++) {
         if (strcmp(hotel[i].nome, nome) == 0) {
             strcpy(hotel[i].nome, novoNome);
@@ -124,38 +142,49 @@ void editarHospede(char *nome, char *novoNome, int novoQuarto) {
             hotel[i].quarto = novoQuarto;
             quartos[temp]--;
             quartos[novoQuarto]++;
-            
+
             printf("Hóspede editado: %s, Quarto: %d\n", hotel[i].nome, hotel[i].quarto);
             encontrado = 1;
             break;
         }
     }
-    
+
 
     if (!encontrado) {
         printf("Hóspede %s não encontrado.\n", nome);
     }
 }
 
+// Função para liberar o quarto, removendo todos os hóspedes que nele estão. Primeiro ela verifica se há hospedes no quarto para depois remover.
 void liberarQuarto(int quarto) {
-    int i;
-    int encontrado = 0;
+    int i, j;
+    int numHospedes = 0;
     for (i = 0; i < totalHospedes; i++) {
         if (hotel[i].quarto == quarto) {
-            memmove(&hotel[i], &hotel[i + 1], (totalHospedes - i - 1) * sizeof(Hospede));
-            totalHospedes--;
-            encontrado = 1;
-        }
-    }
+            numHospedes++;
+        } 
 
-    if (encontrado) {
-        printf("Quarto %d liberado.\n", quarto);
-        quartos[quarto] = 0;
-    } else {
-        printf("Quarto %d não está ocupado.\n", quarto);
     }
+    if (numHospedes == 0) {
+        printf("Não há hóspedes no quarto %d.\n", quarto);
+        return;
+
+    }
+    for (i = 0, j = 0; i < totalHospedes; i++) {
+        if (hotel[i].quarto != quarto) {
+            hotel[j] = hotel[i];
+            j++;
+
+        }
+
+    }
+    totalHospedes -= numHospedes;
+    // define o quarto como 0
+    quartos[quarto] = 0;
+    printf("Todos os hóspedes do quarto %d foram removidos.\n", quarto);
 }
 
+// Função para salvar os hóspedes em um arquivo txt.
 void salvarHospedes() {
     FILE *file = fopen("listadehospedes.txt", "w");
     int i;
@@ -174,6 +203,7 @@ void salvarHospedes() {
     printf("Lista de hóspedes salva com sucesso em 'listadehospedes.txt'.\n");
 }
 
+// Função para atualizar a lista de hóspedes no arquivo txt. É chamada toda vez que há uma atualização na estrutura de hóspedes.
 void atualizarLista() {
     FILE *file = fopen("hospedes.txt", "w");
     int i;
@@ -192,6 +222,7 @@ void atualizarLista() {
     printf("Lista de hóspedes atualizada.\n");
 }
 
+// Função utilizada para limpar o arquivo de hóspedes toda vez que a opção 8 (sair do programa) do menu for chamada.
 void limparArquivo() {
     FILE *file = fopen("hospedes.txt", "w");
 
@@ -205,12 +236,14 @@ void limparArquivo() {
     printf("Dados do arquivo 'hospedes.txt' foram apagados.\n");
 }
 
+
 int main() {
     char nome[MAX_NOME];
     int quarto;
     char nomeNovo[MAX_NOME];
     int opcao;
-    lerHospede();
+    lerHospede(); // toda vez que o programa inicia, é feito a leitura do arquivo de hóspedes e inserido no hotel.
+
 do {
       printf("Menu do Sistema de Registro de Hóspedes:\n");
       printf("1. Inserir hóspede\n"); 
@@ -225,6 +258,7 @@ do {
       scanf("%d", &opcao);
       getchar();
 
+  // switch case com a opção escolhida pelo usuário.
 switch (opcao) {
         case 1:
           printf("Digite o nome do hóspede: ");
@@ -272,6 +306,6 @@ switch (opcao) {
           printf("Opção inválida. Tente novamente.\n");
           break; 
       } 
-  } while (opcao != 8);
+  } while (opcao != 8); // enquanto a opção for diferente de 8, o menu será exibido novamente.
   return 0;
 }
